6b88b67529080fe6d5cddb9aee35c9aad8f4a8ae
KroksasC <gustascemerka@gmail.com>
2025-03-04 23:48:55 +0200

    Add ASP.NET Core budget management application
    This commit introduces a new ASP.NET Core web application for managing a budget, featuring categories and transactions. Key changes include:
    
    - Creation and configuration of solution and project files.
    - Updates to `appsettings.json` for logging and database connection.
    - Targeting .NET 8.0 in the project file with necessary package references.
    - Configuration of services in `Program.cs`, including database context and Swagger.
    - Addition of `CategoryController` and `TransactionController` for CRUD operations.
    - Implementation of `GenericDbContext` for database management.
    - Migration files added for initial database schema setup.
    - Definition of `Category` and `Transaction` model classes.
    - Modifications to frontend files (`index.html`, `index.css`, `Category.js`, `Transaction.js`) for user interface functionality.
    - Configuration of `launchSettings.json` for different run profiles.

---
+++ BudgetApp.KroksasC/BudgetApp.KroksasC/Controllers/CategoryController.cs
@@ -0,0 +1,99 @@
+﻿using BudgetApp.KroksasC.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel;
+using TodoList.KroksasC.Data;
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BudgetApp.KroksasC.Controllers
+{
+    [Route("api/category")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly GenericDbContext _context;
+
+        public CategoryController(GenericDbContext context) => _context = context;
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Category>>> GetCategories()
+        {
+            var categories = _context.Categories;
+
+            var categoriesDto = categories.Select(c => new CategoryDto
+            {
+                Id = c.Id,
+                CategoryName = c.CategoryName,
+            });
+
+            return Ok(categoriesDto);
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Category>> GetCategory(int id)
+        {
+            var category = await _context.Categories.FindAsync(id);
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return category;
+        }
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCategory(int id, Category category)
+        {
+            if (id != category.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Categories.Entry(category).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CategoryExist(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return NoContent();
+        }
+        [HttpPost]
+        public async Task<ActionResult<Category>> PostCategory(Category category)
+        {
+            _context.Categories.Add(category);
+
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(PostCategory), new { id = category.Id }, category);
+        }
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCategory(int id)
+        {
+            var todo = await _context.Categories.FindAsync(id);
+            if (todo == null)
+            {
+                return NotFound();
+            }
+
+            _context.Categories.Remove(todo);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+        private bool CategoryExist(int id)
+        {
+            return _context.Categories.Any(e => e.Id == id);
+        }
+    }
+
+}
---
+++ BudgetApp.KroksasC/BudgetApp.KroksasC/wwwroot/js/Transaction.js
@@ -0,0 +1,302 @@
+﻿const transactionUrl = 'api/transaction';
+
+let transactions = [];
+
+document.addEventListener('DOMContentLoaded', () => {
+    getTransactionData();
+    populateCategoryFilter();
+});
+
+function openTransactionInput() {
+    document.getElementById("add-transaction-form").style.display = "block";
+}
+function closeTransactionInput() {
+    document.getElementById("add-transaction-form").style.display = "none";
+}
+
+function closeTransactionEditForm() {
+    document.getElementById("edit-transaction-form").style.display = "none";
+}
+
+function getTransactionData() {
+    fetch(transactionUrl)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            transactions = data;
+            _displayTransactions();
+        })
+        .catch(error => console.error('Error:', error));
+}
+
+function addTransaction() {
+    const addAmountTextBox = document.getElementById('add-amount');
+    const addDescriptionTextBox = document.getElementById('add-description');
+    const addDateTimeTextBox = document.getElementById('add-date');
+    const selectedCategoryRadio = document.querySelector('input[name="category"]:checked');
+
+    const amount = parseFloat(addAmountTextBox.value.trim());
+    const transactionDate = addDateTimeTextBox.value.trim();
+    const description = addDescriptionTextBox.value.trim();
+    const categoryId = selectedCategoryRadio ? parseInt(selectedCategoryRadio.value) : null;
+
+    if (isNaN(amount) || amount <= 0) {
+        alert("Amount must be a positive number.");
+        return;
+    }
+
+    if (!transactionDate) {
+        alert("Transaction date is required.");
+        return;
+    }
+
+    if (description.length > 500) {
+        alert("Description cannot be longer than 500 characters.");
+        return;
+    }
+
+    if (!categoryId) {
+        alert("Category is required.");
+        return;
+    }
+
+    const transaction = {
+        amount: amount,
+        transactionDate: transactionDate,
+        description: description,
+        category: {
+            id: categoryId
+        }
+    }
+
+    fetch(transactionUrl, {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(transaction)
+    })
+        .then(response => response.json())
+        .then(() => {
+            console.log("Transaction added:", transaction);
+            getTransactionData();
+        })
+        .catch(error => console.error('Unable to add item.', error));
+}
+
+function deleteTransaction(id) {
+    fetch(`${transactionUrl}/${id}`, {
+        method: 'DELETE'
+    })
+        .then(() => getTransactionData())
+        .catch(error => console.error('Unable to delete item.', error));
+}
+
+function displayTransactionEditForm(id) {
+    const transaction = transactions.find(item => item.id === id);
+
+    document.getElementById('edit-amount').value = transaction.amount;
+    document.getElementById('edit-description').value = transaction.description;
+    _displayCategoriesInTransactionsEditForm(transaction);
+    document.getElementById('edit-date').value = transaction.transactionDate.slice(0, 16);
+    document.getElementById('edit-transaction-form').style.display = 'block';
+    const saveButton = document.getElementById("save-transaction-btn");
+
+    // Ensure only one event listener is added (avoiding multiple triggers)
+    saveButton.onclick = function () {
+        updateTransaction(id);
+    };
+}
+
+function updateTransaction(index) {
+    const newAmountTextBox = document.getElementById('edit-amount');
+    const newDescriptionTextBox = document.getElementById('edit-description');
+    const newCategorySelected = document.querySelector('input[name="category1"]:checked');
+    const newDateSelected = document.getElementById('edit-date');
+
+    const amount = parseFloat(newAmountTextBox.value.trim());
+    const transactionDate = newDateSelected.value.trim();
+    const description = newDescriptionTextBox.value.trim();
+    const categoryId = newCategorySelected ? parseInt(newCategorySelected.value) : null;
+
+    if (isNaN(amount) || amount <= 0) {
+        alert("Amount must be a positive number.");
+        return;
+    }
+
+    if (!transactionDate) {
+        alert("Transaction date is required.");
+        return;
+    }
+
+    if (description.length > 500) {
+        alert("Description cannot be longer than 500 characters.");
+        return;
+    }
+
+    if (!categoryId) {
+        alert("Category is required.");
+        return;
+    }
+
+    const transaction = {
+        id: index,
+        amount: amount,
+        transactionDate: transactionDate,
+        description: description,
+        category: {
+            id: categoryId
+        }
+    }
+
+    console.log("Transaction in update:", transaction);
+
+    fetch(`${transactionUrl}/${index}`, {
+        method: 'PUT',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(transaction)
+    })
+        .then(() => {
+            getTransactionData();
+        })
+        .catch(error => console.error('Unable to update item.', error));
+}
+
+function _displayTransactions() {
+    const tBody = document.getElementById('transactions');
+    tBody.innerHTML = ''; // Clear existing rows
+    console.log("Transactions in display:", transactions);
+    transactions.forEach(transaction => {
+        let tr = tBody.insertRow();
+
+        let td1 = tr.insertCell(0);
+        let textNode1 = document.createTextNode(transaction.amount);
+        td1.appendChild(textNode1);
+
+        let td2 = tr.insertCell(1);
+
+        // Convert the ISO date string to a JavaScript Date object
+        let date = new Date(transaction.transactionDate);
+
+        // Format the date as "YYYY-MM-DD HH:mm:ss"
+        let formattedDate = date.toLocaleString("en-US", {
+            year: "numeric",
+            month: "2-digit",
+            day: "2-digit",
+            hour: "2-digit",
+            minute: "2-digit",
+            second: "2-digit",
+            hour12: false // Use `true` for AM/PM format
+        });
+
+        // Create text node with formatted date and append to the cell
+        let textNode2 = document.createTextNode(formattedDate);
+        td2.appendChild(textNode2);
+
+        let td3 = tr.insertCell(2);
+        let description = transaction.description;
+        td3.appendChild(description == null || description === "string" ? document.createTextNode("No description provided") : document.createTextNode(description));
+
+        let td4 = tr.insertCell(3);
+        let textNode4 = document.createTextNode(transaction.category.categoryName);
+        td4.appendChild(textNode4);
+
+        let td5 = tr.insertCell(4);
+        let editButton = document.createElement('button');
+        editButton.innerText = 'Edit';
+        editButton.setAttribute('onclick', `displayTransactionEditForm(${transaction.id})`);
+
+        let deleteButton = document.createElement('button');
+        deleteButton.innerText = 'Delete';
+        deleteButton.setAttribute('onclick', `deleteTransaction(${transaction.id})`);
+
+        td5.appendChild(editButton);
+        td5.appendChild(deleteButton);
+    });
+}
+function filterTransactions() {
+    const searchDescription = document.getElementById('search-description').value.trim().toLowerCase();
+    const filterCategory = document.getElementById('filter-category').value;
+    const filterDate = document.getElementById('filter-date').value;
+
+    const filteredTransactions = transactions.filter(transaction => {
+        const matchesDescription = transaction.description.toLowerCase().includes(searchDescription);
+        const matchesCategory = !filterCategory || transaction.category.id == filterCategory;
+        const matchesDate = !filterDate || transaction.transactionDate.includes(filterDate);
+
+        return matchesDescription && matchesCategory && matchesDate;
+    });
+
+    _displayFilteredTransactions(filteredTransactions);
+}
+
+function _displayFilteredTransactions(filteredTransactions) {
+    const tBody = document.getElementById('transactions');
+    tBody.innerHTML = ''; // Clear existing rows
+    console.log("Filtered transactions in display:", filteredTransactions);
+    filteredTransactions.forEach(transaction => {
+        let tr = tBody.insertRow();
+
+        let td1 = tr.insertCell(0);
+        let textNode1 = document.createTextNode(transaction.amount);
+        td1.appendChild(textNode1);
+
+        let td2 = tr.insertCell(1);
+
+        let date = new Date(transaction.transactionDate);
+
+        let formattedDate = date.toLocaleString("en-US", {
+            year: "numeric",
+            month: "2-digit",
+            day: "2-digit",
+            hour: "2-digit",
+            minute: "2-digit",
+            second: "2-digit",
+            hour12: false // Use `true` for AM/PM format
+        });
+
+        // Create text node with formatted date and append to the cell
+        let textNode2 = document.createTextNode(formattedDate);
+        td2.appendChild(textNode2);
+
+        let td3 = tr.insertCell(2);
+        let description = transaction.description;
+        td3.appendChild(description == null || description === "string" ? document.createTextNode("No description provided") : document.createTextNode(description));
+
+        let td4 = tr.insertCell(3);
+        let textNode4 = document.createTextNode(transaction.category.categoryName);
+        td4.appendChild(textNode4);
+
+        let td5 = tr.insertCell(4);
+        let editButton = document.createElement('button');
+        editButton.innerText = 'Edit';
+        editButton.setAttribute('onclick', `displayTransactionEditForm(${transaction.id})`);
+
+        let deleteButton = document.createElement('button');
+        deleteButton.innerText = 'Delete';
+        deleteButton.setAttribute('onclick', `deleteTransaction(${transaction.id})`);
+
+        td5.appendChild(editButton);
+        td5.appendChild(deleteButton);
+    });
+}
+
+function populateCategoryFilter() {
+    const filterCategorySelect = document.getElementById('filter-category');
+    filterCategorySelect.innerHTML = '<option value="">All categories</option>';
+
+    categories.forEach(category => {
+        const option = document.createElement('option');
+        option.value = category.id;
+        option.textContent = category.categoryName;
+        filterCategorySelect.appendChild(option);
+    });
+}
